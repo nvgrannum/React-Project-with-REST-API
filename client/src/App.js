5bdc500822150faa6b73ffcad96bc7da890caf6a
nvgrannum <79998374+nvgrannum@users.noreply.github.com>
2021-05-25 11:16:51 -0700

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import axios from 'axios'
+//import axios from 'axios'
 
 //const express = require('express');
-
+const axios = require('axios')
 
 class App extends Component{
   
@@ -11,13 +11,23 @@ class App extends Component{
   }
 
   componentDidMount() {
+      this.getUser();
       this.test();
     }
 
   test = async () => {
     await axios.get(`localhost:5000/api/courses`)
-      .then(resultsData => {this.setState({courses:resultsData.data})})
+      .then(resultsData => {this.setState({courses:resultsData})})
       .catch(error=> console.log('error fetching data', error));
+  }
+
+  getUser = async function() {
+    try {
+      const response = await axios.get('localhost:5000/api/courses');
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   render(){
